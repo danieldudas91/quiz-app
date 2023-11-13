a4f477fe6ec09c36a88f722e61f42906d239408b
dd <duddaniel0919@gmail.com>
2023-11-13 19:17:18 +0100

    add companion object constant

--- app/src/main/java/com/example/quizapp/model/Question.kt
+++ app/src/main/java/com/example/quizapp/model/Question.kt
@@ -1,5 +1,7 @@
 package com.example.quizapp.model
 
 class Question (val questionString: String, val answers: List<Answer>){
-
+    companion object{
+        const val MAX_NUMBER_OF_ANSWERS = 4
+    }
 }
