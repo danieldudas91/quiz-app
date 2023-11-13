977952a423b6d23c3ded5b126778104b7e8db793
dd <duddaniel0919@gmail.com>
2023-11-13 19:18:38 +0100

    implement hasFourAnswers and hasOnlyOneCorrectAnswer methods

--- app/src/main/java/com/example/quizapp/model/Question.kt
+++ app/src/main/java/com/example/quizapp/model/Question.kt
@@ -4,4 +4,12 @@ class Question (val questionString: String, val answers: List<Answer>){
     companion object{
         const val MAX_NUMBER_OF_ANSWERS = 4
     }
+
+    private fun hasFourAnswers(): Boolean{
+        return answers.size == MAX_NUMBER_OF_ANSWERS
+    }
+
+    private fun hasOnlyOneCorrectAnswer(): Boolean{
+        return answers.filter{it.isTrueAnswer}.size == 1
+    }
 }
