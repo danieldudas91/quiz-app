90ce489f68a73422650c34aed4db6f0884ac78de
dd <duddaniel0919@gmail.com>
2023-11-13 19:21:59 +0100

    initialize _questions and currentQuestion properties and implement getMutableQuestions function

--- app/src/main/java/com/example/quizapp/model/Game.kt
+++ app/src/main/java/com/example/quizapp/model/Game.kt
@@ -1,5 +1,11 @@
 package com.example.quizapp.model
 
 class Game (private val questions: List<Question>) {
+    private val _questions: MutableList<Question> = getMutableQuestions()
+    private lateinit var  currentQuestion: Question
 
+
+    private fun getMutableQuestions(): MutableList<Question> {
+        return questions.filter { it.isProperQuestion }.shuffled().toMutableList()
+    }
 }
