e18b39ed6da95c3b40d45d49882a046117440b48
dd <duddaniel0919@gmail.com>
2023-11-13 19:23:03 +0100

    implement getFirstQuestion and getNextQuestion functions

--- app/src/main/java/com/example/quizapp/model/Game.kt
+++ app/src/main/java/com/example/quizapp/model/Game.kt
@@ -4,8 +4,32 @@ class Game (private val questions: List<Question>) {
     private val _questions: MutableList<Question> = getMutableQuestions()
     private lateinit var  currentQuestion: Question
 
-
     private fun getMutableQuestions(): MutableList<Question> {
         return questions.filter { it.isProperQuestion }.shuffled().toMutableList()
     }
+
+    fun getFirstQuestion(): Question{
+        val firstQuestion = try{
+            _questions.first()
+        }
+        catch (e: NoSuchElementException){
+            e.printStackTrace()
+        }
+        currentQuestion = firstQuestion as Question
+        return firstQuestion
+    }
+
+    fun getNextQuestion(): Question{
+        _questions.remove(currentQuestion)
+        var nextQuestion = Question("", listOf())
+        try{
+            nextQuestion = _questions.first()
+        }
+        catch(e: NoSuchElementException){
+            e.printStackTrace()
+        }
+        currentQuestion = nextQuestion
+        return  nextQuestion
+    }
+
 }
