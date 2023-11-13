cfff7777136db38d5cd16be1f67ffafc58a77454
dd <duddaniel0919@gmail.com>
2023-11-13 19:20:25 +0100

    implement determineIsProperQuestion and isProperQuestion property

--- app/src/main/java/com/example/quizapp/model/Question.kt
+++ app/src/main/java/com/example/quizapp/model/Question.kt
@@ -5,6 +5,12 @@ class Question (val questionString: String, val answers: List<Answer>){
         const val MAX_NUMBER_OF_ANSWERS = 4
     }
 
+    var isProperQuestion: Boolean = determineIsProperQuestion()
+
+    private fun determineIsProperQuestion(): Boolean{
+        return hasFourAnswers() && hasOnlyOneCorrectAnswer()
+    }
+
     private fun hasFourAnswers(): Boolean{
         return answers.size == MAX_NUMBER_OF_ANSWERS
     }
