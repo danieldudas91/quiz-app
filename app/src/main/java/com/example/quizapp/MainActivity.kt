045c8f13d8dbdb8cf4812b1320498a9e22c05212
dd <duddaniel0919@gmail.com>
2023-11-14 14:10:57 +0100

    implement basic ui

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.example.quizapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.text.AnnotatedString
+import com.example.quizapp.model.Answer
+import com.example.quizapp.model.Game
+import com.example.quizapp.model.Question
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +34,22 @@ class MainActivity : ComponentActivity() {
             Question("Which one of the following is one of Mars' moons?", secondQuestionAnswers)
         )
         setContent {
+            Column {
+                val game = Game(questions)
+                val context = applicationContext
+                val currentQuestion = remember { mutableStateOf(game.getFirstQuestion()) }
+                Text(currentQuestion.value.questionString)
+                currentQuestion.value.answers.forEach { answer ->
+                    ClickableText(text = AnnotatedString(answer.answerString),
+                        onClick = {
+                            if (answer.isTrueAnswer) {
+                                currentQuestion.value = game.getNextQuestion()
+                            } else {
+                                Toast.makeText(context, "Not true", Toast.LENGTH_LONG).show()
+                            }
+                        })
+                }
+            }
         }
     }
 }
