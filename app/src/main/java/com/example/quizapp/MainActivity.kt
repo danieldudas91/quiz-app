2b031298c6d754029fffcbf7220f88c154516519
dd <duddaniel0919@gmail.com>
2023-11-14 14:10:05 +0100

    add questions and answers for testing

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -7,6 +7,22 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val firstQuestionAnswers = listOf(
+            Answer("Sydney", false),
+            Answer("Perth", false),
+            Answer("Canberra", true),
+            Answer("Melbourne", false)
+        )
+        val secondQuestionAnswers = listOf(
+            Answer("Io", false),
+            Answer("Phobos", true),
+            Answer("Europa", false),
+            Answer("Titan", false)
+        )
+        val questions = listOf(
+            Question("What is the capital of Australia?", firstQuestionAnswers),
+            Question("Which one of the following is one of Mars' moons?", secondQuestionAnswers)
+        )
         setContent {
         }
     }
