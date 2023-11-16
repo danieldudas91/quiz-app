2ff1a871f48c7a42cf06c67d7a0a1f36bd6c7446
dd <duddaniel0919@gmail.com>
2023-11-16 16:58:24 +0100

    rename id to answerId and add questionId

--- app/src/main/java/com/example/quizapp/model/Answer.kt
+++ app/src/main/java/com/example/quizapp/model/Answer.kt
@@ -1,10 +1,18 @@
 package com.example.quizapp.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "answers")
-class Answer (val answerString: String,
-              val isTrueAnswer:Boolean){
-    @PrimaryKey(autoGenerate = true) val id: Int? = null
+class Answer (@ColumnInfo(name = "answer_string")
+              val answerString: String,
+              @ColumnInfo(name = "is_true_answer")
+              val isTrueAnswer:Boolean,
+              @ColumnInfo(name = "question_id")
+              var questionId: Int? = null
+){
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "answer_id")
+    var answerId: Int? = null
 }
