c5d6843822ef6a6865d138d9dc1f9e30297da4c3
dd <duddaniel0919@gmail.com>
2023-11-16 16:59:12 +0100

    remove unnecessary functions and rename id to questionId

--- app/src/main/java/com/example/quizapp/model/Question.kt
+++ app/src/main/java/com/example/quizapp/model/Question.kt
@@ -5,30 +5,19 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "questions")
-data class Question (
-                     val questionString: String,
-
-                     val answers: List<Answer>)
-{
+data class Question(
+    @ColumnInfo(name = "question_string")
+    val questionString: String,
+) {
     @PrimaryKey(autoGenerate = true)
-    var id: Int? = null
-    companion object{
+    @ColumnInfo(name = "question_id")
+    var questionId: Int? = null
+
+    companion object {
         const val MAX_NUMBER_OF_ANSWERS = 4
     }
 
 
     @ColumnInfo(name = "is_proper_question")
-    var isProperQuestion: Boolean = determineIsProperQuestion()
-
-    private fun determineIsProperQuestion(): Boolean{
-        return hasFourAnswers() && hasOnlyOneCorrectAnswer()
-    }
-
-    private fun hasFourAnswers(): Boolean{
-        return answers.size == MAX_NUMBER_OF_ANSWERS
-    }
-
-    private fun hasOnlyOneCorrectAnswer(): Boolean{
-        return answers.filter{it.isTrueAnswer}.size == 1
-    }
+    var isProperQuestion: Boolean = true
 }
