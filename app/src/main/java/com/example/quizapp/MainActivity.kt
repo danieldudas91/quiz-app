31848619706a579d2ddf37f1387fd9c226a67306
dd <duddaniel0919@gmail.com>
2023-11-16 17:09:34 +0100

    refactor code

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -10,40 +10,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.text.AnnotatedString
-import com.example.quizapp.model.Answer
 import com.example.quizapp.model.Game
-import com.example.quizapp.model.Question
+import com.example.quizapp.viewModel.QuizViewModel
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val firstQuestionAnswers = listOf(
-            Answer("Sydney", false),
-            Answer("Perth", false),
-            Answer("Canberra", true),
-            Answer("Melbourne", false)
-        )
-        val secondQuestionAnswers = listOf(
-            Answer("Io", false),
-            Answer("Phobos", true),
-            Answer("Europa", false),
-            Answer("Titan", false)
-        )
-        val questions = listOf(
-            Question("What is the capital of Australia?", firstQuestionAnswers),
-            Question("Which one of the following is one of Mars' moons?", secondQuestionAnswers)
-        )
+        val viewModel = QuizViewModel(applicationContext)
+        val questions = viewModel.getAllQuestionsWithAnswers()
+        val game = questions?.let { Game(it) }
         setContent {
             Column {
-                val game = Game(questions)
                 val context = applicationContext
-                val currentQuestion = remember { mutableStateOf(game.getFirstQuestion()) }
-                Text(currentQuestion.value.questionString)
-                currentQuestion.value.answers.forEach { answer ->
+                val currentQuestionState = remember { mutableStateOf(game?.getFirstQuestion()) }
+                val currentQuestionValue = currentQuestionState.value
+                Text(currentQuestionValue!!.question.questionString)
+                currentQuestionValue.answers.forEach { answer ->
                     ClickableText(text = AnnotatedString(answer.answerString),
                         onClick = {
                             if (answer.isTrueAnswer) {
-                                currentQuestion.value = game.getNextQuestion()
+                                currentQuestionState.value = game!!.getNextQuestion()
                             } else {
                                 Toast.makeText(context, "Not true", Toast.LENGTH_LONG).show()
                             }
