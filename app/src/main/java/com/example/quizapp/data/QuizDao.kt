c758d77e4ff05067359bdb770e5fd474869d0783
dd <duddaniel0919@gmail.com>
2023-11-16 13:12:31 +0100

    add getAllQuestionsWithAnswers and addQuestionWithAnswers functions

--- app/src/main/java/com/example/quizapp/data/QuizDao.kt
+++ app/src/main/java/com/example/quizapp/data/QuizDao.kt
@@ -4,12 +4,22 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import com.example.quizapp.model.Question
+import com.example.quizapp.model.QuestionWithAnswers
 
 @Dao
 interface QuizDao {
     @Query("SELECT * FROM questions")
     fun getAllQuestions(): List<Question>
+
+    @Transaction
+    @Query("SELECT * FROM questions")
+    suspend fun getAllQuestionsWithAnswers(): List<QuestionWithAnswers>
+
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addQuestionWithAnswers(question: Question)
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun addQuestion(question: Question)
