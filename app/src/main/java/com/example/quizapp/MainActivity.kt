8a29a203580a30cf8909bdf10a9c9d7aacd57bae
dd <duddaniel0919@gmail.com>
2024-01-22 11:51:30 +0100

    introduce AnswerText and QuizComponent composable functions

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,34 +1,34 @@
 package com.example.quizapp
 
-import android.graphics.drawable.shapes.RoundRectShape
+import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.quizapp.model.Answer
 import com.example.quizapp.model.Game
+import com.example.quizapp.model.QuestionWithAnswers
 import com.example.quizapp.viewModel.QuizViewModel
 
 class MainActivity : ComponentActivity() {
@@ -39,44 +39,59 @@ class MainActivity : ComponentActivity() {
         val questions = viewModel.getAllQuestionsWithAnswers()
         val game = questions?.let { Game(it) }
         setContent {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                val context = applicationContext
-                val currentQuestionState = remember { mutableStateOf(game?.getFirstQuestion()) }
-                val currentQuestionValue = currentQuestionState.value
-                Text(
-                    text = currentQuestionValue!!.question.questionString,
-                    modifier = Modifier
-                        .width(300.dp)
-                        .padding(all = 10.dp)
-                        .border(width = 1.dp, color = Color.Black, shape = CircleShape),
-                    textAlign = TextAlign.Center,
+            if (game != null) {
+                QuizComponent(game = game)
+            }
+        }
+    }
 
+    @Composable
+    fun QuizComponent(game: Game){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val currentQuestionState = remember { mutableStateOf(game.getFirstQuestion()) }
+            val currentQuestionValue = currentQuestionState.value
+            Text(
+                text = currentQuestionValue.question.questionString,
+                fontSize = 24.sp,
+                modifier = Modifier
+                    .width(350.dp)
+                    .padding(all = 10.dp)
+                    .border(width = 1.dp, color = Color.Black, shape = CircleShape),
+                textAlign = TextAlign.Center,
                 )
-                currentQuestionValue.answers.forEach { answer ->
-                    Box(
-                        modifier = Modifier
-                            .width(100.dp)
-                            .padding(all = 5.dp)
-                            .border(width = 1.dp, color = Color.Black, shape = CircleShape)
-                    ) {
-                        ClickableText(
-                            text = AnnotatedString(answer.answerString),
-                            onClick = {
-                                if (answer.isTrueAnswer) {
-                                    currentQuestionState.value = game!!.getNextQuestion()
-                                } else {
-                                    Toast.makeText(context, "Not true", Toast.LENGTH_LONG).show()
-                                }
-                            },
-                            modifier = Modifier
-                                .align(Alignment.Center)
-                        )
-                    }
+            currentQuestionValue.answers.forEach { answer ->
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .width(200.dp)
+                        .padding(all = 5.dp)
+                        .border(width = 1.dp, color = Color.Black, shape = CircleShape),
+                ) {
+                    AnswerText(answer, currentQuestionState, game, applicationContext)
                 }
             }
         }
+    }
+
+    @Composable
+    fun AnswerText(answer: Answer,
+                   currentQuestionState: MutableState<QuestionWithAnswers>,
+                   game: Game,
+                   context: Context
+                   ){
+        ClickableText(
+            text = AnnotatedString(answer.answerString),
+            style = TextStyle(fontSize = 24.sp),
+            onClick = {
+            if (answer.isTrueAnswer) {
+                currentQuestionState.value = game.getNextQuestion()
+            } else {
+                Toast.makeText(context, "Not true", Toast.LENGTH_LONG).show()
+            }
+        },
+        )
     }
 }
